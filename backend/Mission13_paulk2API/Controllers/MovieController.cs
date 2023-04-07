323030334aea77a66f6cf6ca3b46bfe0582fff3a
Kevin Paul <paulk2@byu.edu>
2023-04-06 18:41:43 -0600

    initial

---
+++ backend/Mission13_paulk2API/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission13_paulk2API.Data;
+
+namespace Mission13_paulk2API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+
+        private JoelHiltonMovieCollectionContext context;
+
+        public MovieController(JoelHiltonMovieCollectionContext temp)
+        {
+            context = temp;
+        } 
+
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies
+                .Where(m => m.Edited == "Yes")
+                .OrderBy(m => m.Title)
+                .ToArray();
+        }
+    }
+}
---
+++ backend/Mission13_paulk2API/Data/JoelHiltonMovieCollectionContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Mission13_paulk2API.Data
+{
+    public class JoelHiltonMovieCollectionContext : DbContext
+    {
+        public JoelHiltonMovieCollectionContext (DbContextOptions<JoelHiltonMovieCollectionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
